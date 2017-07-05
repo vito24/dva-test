2d56d9ca21c4bbc6a43659deb18e2ebf730fc8f0
vito <583013043@qq.com>
2017-07-05 09:34:36 +0800

    update

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Table, Pagination, Popconfirm } from 'antd';
+import { Table, Pagination, Popconfirm, Button } from 'antd';
 import { routerRedux } from 'dva/router';
 import styles from './Users.css';
 import { PAGE_SIZE } from '../../constants';
@@ -20,6 +20,14 @@ function Users({ dispatch, list: dataSource, loading, total, page: current }) {
     dispatch({
       type: 'users/patch',
       payload: { id, values },
+    });
+  }
+
+  // 新增
+  function createHandler(values) {
+    dispatch({
+      type: 'users/create',
+      payload: values,
     });
   }
 
@@ -67,6 +75,11 @@ function Users({ dispatch, list: dataSource, loading, total, page: current }) {
   return (
     <div className={styles.normal}>
       <div>
+        <div className={styles.create}>
+          <UserModal record={{}} onOk={createHandler}>
+            <Button type="primary">Create User</Button>
+          </UserModal>
+        </div>
         <Table
           columns={columns}
           dataSource={dataSource}
